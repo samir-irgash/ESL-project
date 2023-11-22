141ad274e5d9c41fbed13504182f1826837726fa
user <irgashevsamir982@gmail.com>
2023-11-22 14:41:19 +0000

    Related to #5; 'Static' context added to be able to control multiple LEDs in the next WS

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include "nrf_delay.h"
 #include "boards.h"
 #include "nrfx_systick.h"
 #include "nrfx_gpiote.h"
@@ -11,11 +10,16 @@
 #include "nrf_drv_clock.h"
 #include "my_switch.h"
 #include "my_pwm.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "nrf_log_backend_usb.h"
 
-#define DEVICE_ID 1111
+#define DEVICE_ID 1234
 #define SMALL_DELAY_MS 500
 
 const uint32_t leds[LEDS_NUMBER] = {LED_RED, LED_GREEN, LED_BLUE, LED_0};
+const int sequence[LEDS_NUMBER] = {(DEVICE_ID/1000)%10, (DEVICE_ID/100)%10, (DEVICE_ID/10)%10, DEVICE_ID%10};
 volatile bool pause;
 
 /**@brief Function starting the internal LFCLK oscillator.
@@ -30,15 +34,7 @@ void lfclk_request(void)
     nrf_drv_clock_lfclk_request(NULL);
 }
 
-void blink_n_times(uint32_t led_idx, int n) {
-    
-    for (size_t i = 0; i < n; ++i) {
-        my_pwm_blink(led_idx);
-        nrf_delay_ms(100);
-    }
-}
-
-void initialize(void) {
+void init(void) {
     bsp_board_init(BSP_INIT_LEDS);
     my_gpio_config_sw1();
     my_gpio_config_leds();
@@ -46,6 +42,9 @@ void initialize(void) {
     lfclk_request();
     app_timer_init();
     nrfx_gpiote_init();
+    // ret_code_t ret_code = NRF_LOG_INIT(NULL);
+    // APP_ERROR_CHECK(ret_code);
+    // NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
 /**
@@ -53,19 +52,23 @@ void initialize(void) {
  */
 int main(void)
 {
-    initialize();
+    init();
     my_switch_init();
 
     /* Toggle LEDs. */
     while (true)
     {
-        blink_n_times(leds[0], (DEVICE_ID/1000)%10);
-        nrf_delay_ms(SMALL_DELAY_MS);
-        blink_n_times(leds[1], (DEVICE_ID/100)%10);
-        nrf_delay_ms(SMALL_DELAY_MS);
-        blink_n_times(leds[2], (DEVICE_ID/10)%10);
-        nrf_delay_ms(SMALL_DELAY_MS);
-        blink_n_times(leds[3], DEVICE_ID%10);
-        nrf_delay_ms(SMALL_DELAY_MS);
+        for (size_t led_idx = 0; led_idx < LEDS_NUMBER; ++led_idx) {
+            blink_context_t context = {
+                .led_i = leds[led_idx],
+                .duty_cycle_milli = DUTY_CYCLE_MIN_MILLI,
+                .duty_cycle_step_milli = DUTY_CYCLE_STEP_MILLI,
+                .next_exec_time_us = 0,
+                .pwm_on = false,
+                .pwm_state = BLINK_BEGIN,
+                .take_step = true,
+            };
+            blink_n_times(&context, sequence[led_idx]);
+        }
     }
 }
