faf26ce7435d3de7da7c9fd4743c9d5c0db71148
Samir Irgashev <irgashev.sam@gmail.com>
2023-12-27 12:01:42 +0000

    command line interface added, available commands: help, hsv, rgb

---
+++ my_cli/my_command.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "my_command.h"
+
+parsed_command_t parse_string(char *str) {
+    int i, j=0;
+    parsed_command_t command;
+    command.argc = 1;
+    
+    for (i=0; i<strlen(str); i++) {
+        if (str[i] == ' ' || str[i] == '\0') {
+            command.args[command.argc-1][j] = '\0';
+            command.argc++;
+            j = 0;
+        } else {
+            command.args[command.argc-1][j] = str[i];
+            j++;
+        }
+    }
+    
+    return command;
+}
