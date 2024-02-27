29630a3befc9fce6814b4947a9ccfdadecf8c5fd
Samir Irgashev <irgashev.sam@gmail.com>
2024-02-27 18:58:05 +0000

    initial commit

---
+++ pca10059/s113/armgcc/_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
@@ -0,0 +1,7 @@
+_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
+ ../../../../esl-nsdk/external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
+ ../config/sdk_config.h ../config/app_config.h
+
+../config/sdk_config.h:
+
+../config/app_config.h:
