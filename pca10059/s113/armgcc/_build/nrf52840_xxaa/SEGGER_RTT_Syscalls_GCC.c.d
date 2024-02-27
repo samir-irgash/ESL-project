c242979c33e7fa616ffd858a681b89e389ec04f5
Samir Irgashev <irgashev.sam@gmail.com>
2024-02-27 19:08:11 +0000

    gh acc switch

--- pca10059/s113/armgcc/_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,7 +0,0 @@
-_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- ../../../../esl-nsdk/external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- ../config/sdk_config.h ../config/app_config.h
-
-../config/sdk_config.h:
-
-../config/app_config.h:
